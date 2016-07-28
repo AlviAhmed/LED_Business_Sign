93e932fe1b9c5a707b3293a638d26823dbe06bee
Alvi Ahmed <alviahmed1234@gmail.com>
2016-07-27 20:39:26 -0400

    newly made file
    newly made file

---
+++ GccApplication1/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 2016-07-27 8:38:52 PM
+ * Author : Alvi
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
