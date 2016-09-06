5ba78e71a2a3d547483a2dde02add43708bb8c67
Alvi Ahmed <alviahmed1234@gmail.com>
2016-09-05 20:22:48 -0400

    mostly working
    mostly working

--- main.c
+++ main.c
@@ -3,48 +3,68 @@
    #include <avr/io.h>
    #include <stdio.h>
    #include <util/delay.h>
-   #include <avr/interrupt.h> 
- 
+   #include <avr/interrupt.h>
+   
    
    #define tot_ddr DDRD
    #define tot_prt PORTD
    #define data_pin PD5
    #define clock_pin PD6
-   #define latch_pin PD7 
-   #define sec1 PD4 
-   #define sec2 PD3
+   #define latch_pin PD7
+   #define sec1 PD4
+   #define sec2 PD3 
+   #define lst_led PD2
    int count = 0,sec_num = 0;
    
    
    int main (void) {
-	   tot_ddr |= ((1 << data_pin) | (1 << clock_pin) | (1 << latch_pin));
-	   tot_prt &= ~ ((1 << data_pin) | (1 << clock_pin) | (1 << latch_pin));
+	   tot_ddr |= ((1 << data_pin) | (1 << clock_pin) | (1 << latch_pin) | (1 << sec2) | (1 << sec1) | (1 << lst_led));
+	   tot_prt &= ~ ((1 << data_pin) | (1 << clock_pin) | (1 << latch_pin) | (1 << sec1) ); 
+	   tot_prt |= (1 << sec2); 
+	   
 	   TCCR1B |= (1 << WGM12); // ctc mode configuration
 	   TIMSK1 |= (1 << OCIE1A); // enable interrupt
 	   sei();
 	   OCR1A = 6249; //setting compare value
 	   TCCR1B |= (1 << CS12 ); //prescaler 256
 	   while (1)
-	   {
-		   if (count == 0){
-			   tot_prt |= (1 << data_pin);
-			   tot_prt |= (1 << latch_pin);
-		   }
-		   if (count > 0 ){
-			   tot_prt &= ~ (1 << data_pin);
-			   tot_prt |= (1 << latch_pin);
-		   }
+	   { 
+		  
+		   
 	   }
 	   
    }
    
    
-   ISR (TIMER1_COMPA_vect){
-	   tot_prt ^= (1 << clock_pin);
-	   tot_prt &= ~ (1 << latch_pin);
-	   count ++; 
-	   if (count > 33){
-		   count = 0;
-	   }
+   ISR (TIMER1_COMPA_vect){ 
+	   if (count == 0){
+		   tot_prt |= (1 << data_pin); 
+		   tot_prt |= (1 << clock_pin);
+		}
+	   if (count > 0 ){
+		   tot_prt &= ~ (1 << data_pin);
+		   tot_prt |= (1 << clock_pin); 
+		}  
+		tot_prt &= ~ (1 << clock_pin);
+		tot_prt |= (1 << latch_pin); 
+		tot_prt &=~ (1 << latch_pin);
+		count ++; 
+		tot_prt &= ~ (1 << lst_led); 
+		
+	   if (count > 32){ 
+		   tot_prt |= (1 << lst_led); 
+		   if (sec_num == 0 && count > 32){ 
+			     tot_prt &= ~ (1 << sec2); //Turn on the next section of leds
+			     tot_prt |= (1 << sec1); //Turn off the next section of leds
+			   sec_num = 1;
+		   }   
+		   if (sec_num == 1 && count > 32){ 
+			   tot_prt &= ~ (1 << sec1);
+			   tot_prt |= (1 << sec2);
+			   sec_num = 0;
+		   }
+		   count = 0; 
+		   
+		}
 	   
    }
