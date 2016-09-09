cf7008b6165af0e9246592339553551f1a1bc9d4
Alvi Ahmed <alviahmed1234@gmail.com>
2016-09-09 03:48:45 -0400

    basic animation working
    basic animation working

--- main.c
+++ main.c
@@ -14,7 +14,7 @@
    #define sec1 PD4
    #define sec2 PD3 
    #define lst_led PD2
-   int count = 0,sec_num = 0;
+   int count = 0,sec_num = 0,mark=0,bark = 0;
    
    
    int main (void) {
@@ -28,7 +28,21 @@
 	   OCR1A = 6249; //setting compare value
 	   TCCR1B |= (1 << CS12 ); //prescaler 256
 	   while (1)
-	   { 
+	   {  
+		     if (sec_num == 0 && bark == 1){
+			     tot_prt &= ~ (1 << sec2); //Turn on the next section of leds
+			     tot_prt |= (1 << sec1); //Turn off the next section of leds
+			     sec_num = 1; 
+				 mark = 0; 
+				 bark = 0;
+		     }
+		     if (sec_num == 1 && bark == 1){
+			     tot_prt &= ~ (1 << sec1);
+			     tot_prt |= (1 << sec2);
+			     sec_num = 0; 
+				 mark = 0; 
+				 bark = 0;
+		     }
 		  
 		   
 	   }
@@ -36,7 +50,8 @@
    }
    
    
-   ISR (TIMER1_COMPA_vect){ 
+   ISR (TIMER1_COMPA_vect){  
+	   //
 	   if (count == 0){
 		   tot_prt |= (1 << data_pin); 
 		   tot_prt |= (1 << clock_pin);
@@ -44,27 +59,24 @@
 	   if (count > 0 ){
 		   tot_prt &= ~ (1 << data_pin);
 		   tot_prt |= (1 << clock_pin); 
-		}  
+		}   
+		//
 		tot_prt &= ~ (1 << clock_pin);
-		tot_prt |= (1 << latch_pin); 
-		tot_prt &=~ (1 << latch_pin);
+		latchpulse();
 		count ++; 
 		tot_prt &= ~ (1 << lst_led); 
-		
-	   if (count > 32){ 
+		if (mark == 1){
+			bark = 1;
+		}
+		if (count > 32){ 
 		   tot_prt |= (1 << lst_led); 
-		   if (sec_num == 0 && count > 32){ 
-			     tot_prt &= ~ (1 << sec2); //Turn on the next section of leds
-			     tot_prt |= (1 << sec1); //Turn off the next section of leds
-			   sec_num = 1;
-		   }   
-		   if (sec_num == 1 && count > 32){ 
-			   tot_prt &= ~ (1 << sec1);
-			   tot_prt |= (1 << sec2);
-			   sec_num = 0;
-		   }
-		   count = 0; 
-		   
+		 count = 0; 
+		  mark = 1;
 		}
 	   
-   }
+   } 
+   
+   void latchpulse (){
+	   tot_prt |= (1 << latch_pin);
+	   tot_prt &=~ (1 << latch_pin);
+	}
