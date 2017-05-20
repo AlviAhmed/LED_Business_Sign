e23195144bfa645a51e66e259b52c7d6470055d0
Alha <alviahmed1234@gmail.com>
2017-05-20 09:52:50 -0400

    Fixing logic of code, making it work in a simplier way

--- main.c
+++ main.c
@@ -6,21 +6,35 @@
    #include <avr/interrupt.h>
    
    
-   #define tot_ddr DDRD
-   #define tot_prt PORTD
-   #define data_pin PD5
-   #define clock_pin PD6
-   #define latch_pin PD7
-   #define sec1 PD4
-   #define sec2 PD3 
-   #define lst_led PD2
-   int count = 0,sec_num = 0,mark=0,bark = 0;
+   #define ddr1 DDRD
+   #define prt1 PORTD  
+   #define ddr2 DDRC 
+   #define prt2 PORTC
+
+   #define data_pin (1 << PD5)
+   #define clock_pin (1 << PD6) 
+   #define latch_pin (1 << PD7)
+   #define sec1 (1 << PD4)
+   #define sec2 (1 << PD3) 
+   #define sec3 (1 << PC5) 
+   #define sec4 (1 << PC4)
+   #define lst_led (1 << PD2)
+   int index = 0, on_led = 4, lst_lednum = 32, lst_led2num = 65;
+   // Section 1 : LEDS  0-14 
+   // Section 2 : LEDS 15-32 
+   // Section 3 : LEDS 33-47 
+   // Section 4 : LEDS 48-65
    
-   
-   int main (void) {
-	   tot_ddr |= ((1 << data_pin) | (1 << clock_pin) | (1 << latch_pin) | (1 << sec2) | (1 << sec1) | (1 << lst_led));
-	   tot_prt &= ~ ((1 << data_pin) | (1 << clock_pin) | (1 << latch_pin) | (1 << sec1) ); 
-	   tot_prt |= (1 << sec2); 
+   int main (void) { 
+   	//Initialization of Ports
+	ddr1 |= ( data_pin | clock_pin | latch_pin | sec1 | sec2 | lst_led);
+	ddr2 |= ( sec3 | sec4 ); 
+
+	//Initial Values of Variables before main function 
+
+	   prt1 &= ~ ( data_pin | clock_pin | latch_pin | sec2 );  
+	   prt2 &= ~ ( sec3 | sec4 )
+	   prt1 |= (1 << sec1); 
 	   
 	   TCCR1B |= (1 << WGM12); // ctc mode configuration
 	   TIMSK1 |= (1 << OCIE1A); // enable interrupt
